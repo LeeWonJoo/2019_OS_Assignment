ea79f31fa92a32b380b1a3899f32e6896077c062
WonJoo Lee <wonjoo12345@naver.com>
2019-04-11 19:22:03 +0900

    in "Makefile" : 	- student infos edited(New team member!).
    in "lab1_sched_types.h" :
    	- some declarations are edited.
    	- args of functions are edited.
    	- new variable to structure PROCESS(int priority).
    
    in "lab1_sched_test.c" :
    	- NEW global variables!
    	- "insert_process" function added.
    
    in "lab1_sched.c"
    	- NEW global variables!
    	- "sched_RoundRobin" function is DONE!!
    	- "RRpop" function is DONE!!
    	- "RRpush" function is DONE!!
    		>>this will be used for other functions too.
    	- "allocate_memory", "initialize_memory", "copy_data" functions added.

--- lab1_sched/include/lab1_sched_types.h
+++ lab1_sched/include/lab1_sched_types.h
@@ -1,13 +1,13 @@
 /*
 *	DKU Operating System Lab
 *	    Lab1 (Scheduler Algorithm Simulator)
-*	    Student id : 32170896, 32163395
-*	    Student name : Kim EunJin, Lee WonJoo
+*	    Student id : 32170896, 32163395, 32162925
+*	    Student name : Kim EunJin, Lee WonJoo, Yun SeulGi
 *
 *   lab1_sched_types.h :
-*       - lab1 header file.
+*       - Lab1 header file.
 *       - Must contain scheduler algorithm function's declations.
-*       + ALSO contains structure infomation used in the functions
+*       + ALSO contains structure infomation used in the functions.
 *
 */
 
@@ -24,15 +24,30 @@ typedef struct process_ {
 	char name;
 	int t_arrival;
 	int t_service;
-				
+	int t_process; //for checking remaining servicetime
+	int priority; //this is used in Lottery sched!!
 }PROCESS;
 
-int sched_FIFO(PROCESS* p_array, int p_num);
-int sched_RoundRobin(PROCESS* p_array, int p_num);
-int sched_MLFQ(PROCESS* p_array, int p_num);
-int sched_Lottery(PROCESS* p_array, int p_num);
+int sched_FIFO(PROCESS* p_array[]);
+int sched_RoundRobin(PROCESS* p_array[]);
+int sched_MLFQ(PROCESS* p_array[]);
+int sched_Lottery(PROCESS* p_array[]);
+
+//for Dinamic allocation of memory in RR, MLFQ, ...
+void allocate_memory(PROCESS* array[]);
+
+void initialize_memory(PROCESS* array[]); //initialize memory to NULL
+int copy_data(PROCESS** p1, PROCESS** p2); //copying the data
+
+void print_sched(); //how?
+
+//these funcs will be turned into void funcs at the end of the project(not fixed)
+int RRpop(PROCESS* Queue[]); //this is used in rr
+int RRpush(PROCESS* Queue[], PROCESS* p_array[], PROCESS* temp, int index); //this is used in rr
+
+
+
+/*Do we need to >>>>sort the workloads<<<< in t_arrival order? */
 
 #endif /* _LAB1_HEADER_H*/
-
-
 
--- lab1_sched/lab1_sched_test.c
+++ lab1_sched/lab1_sched_test.c
@@ -1,10 +1,8 @@
 /*
 *	DKU Operating System Lab
 *	    Lab1 (Scheduler Algorithm Simulator)
-*	    Student id : 32170896, 32163395
-*	    Student name : Kim EunJin, Lee WonJoo
-*	    Student id : 
-*	    Student name : 
+*	    Student id : 32170896, 32163395, 32162925
+*	    Student name : Kim EunJin, Lee WonJoo, Yun SeulGi
 *
 *   lab1_sched.c :
 *       - Lab1 source file.
@@ -30,41 +28,76 @@
 
 #include "lab1_sched_types.h"
 
+int** result = NULL;
+extern int process_num; //for number of processes
+extern int t_total; //for number of processes
+extern int head;
+extern int tail;
+extern char* char_result;
+
 /*
  * you need to implement scheduler simlator test code.
  *
  */
 
-int main(int argc, char *argv[]){
-	int p_num; //for number of processes
-	PROCESS *p_array; //struct process_
+//function for inserting processe info's into "p_array"
+void insert_process(PROCESS* p_array[]);
 
-	int i; //for loops
+int main(int argc, char *argv[]){
+	//PROCESS *p_array; //struct process_
+
+	//int i; //for loops
 
 	printf("**** Scheduler simulator - FIFO, RR, MLFQ, Lottery ****\n");
 	printf("Number of processes? : ");
-	scanf("%d", &p_num);
-	//Dinamic allocation of memory
-	p_array = (int*)malloc(sizeof(PROCESS) * p_num);
+	scanf("%d", &process_num);
 
-	printf("Input all the names of processes together : ");
+	const int p_num = process_num; //to prevent any changes to process_num
+	PROCESS *p_array[p_num]; //array for PROCESS*(struct process_*)
 
+printf("main ");
+
+	//Dinamic allocation and initialization is done in insertProcess()
+	insert_process(p_array);
+
+	char_result = (char*)calloc(t_total, sizeof(char));
+	
+	/* Now calls all the scheduling simulator */
+	/* You can modify the args of the functions */
+
+	//sched_FIFO(p_array);
+	sched_RoundRobin(p_array);
+	//sched_MLFQ(p_array);
+	//sched_Lottery(p_array);
+
+	return 0;
+}
+
+void insert_process(PROCESS* p_array[])
+{
+	//changes the head and tail value
+	int i; //for loops
+	const int p_num = process_num; //to prevent any changes to process_num
+	
+printf("allocate ");	
+	allocate_memory(p_array);
+
+	//Initialization process
 	for(i=0; i<p_num; i++) {
-		printf("Input the name(single char) of %dth process : ", i);
+		getchar(); //emptys the buffer
+		printf("\nInput the name(single char) of %dth process : ", i);
 		scanf("%c", &p_array[i]->name);
 		printf("Input arrival time of %c : ", p_array[i]->name);
 		scanf("%d", &p_array[i]->t_arrival);
 		printf("Input service time of %c : ", p_array[i]->name);
-		scanf("%d", &p_array[i]->t_service);			
+		scanf("%d", &p_array[i]->t_service);
+		printf("Input priority of %c (for Lotterty sched) : ", p_array[i]->name);
+		scanf("%d", &p_array[i]->priority);
+		//t_process is always initialize with the value of t_service
+		p_array[i]->t_process = p_array[i]->t_service;
+		t_total += p_array[i]->t_service;
 	}
 
-	/* Now calls all the scheduling simulator */
-	/* You can modify the name and args of the functions */
-	
-	//sched_FIFO(p_array, p_num);
-	//sched_RoundRobin(p_array, p_num);
-	//sched_MLFQ(p_array, p_num);
-	//sched_Lottery(p_array, p_num);
-
-	return 0;
+printf("initialize done ");	
+	return;
 }
