d71af5f628364f1c7fbe5b9a912d7f0453d8cd78
LeeWonJoo <wonjoo12345@naver.com>
2019-04-11 19:40:46 +0900

    commit 20190411 (#3)
    * in "lab1_sched_test.c" :
    	-"main" function is in edition.
    
    other :
    	- "test.txt" is created for commit test.
    
    * in "Makefile" :
    	- added student infomations.
    
    in "lab1_sched_types.h" :
    	- added student infomations.
    	- "PROCESS" structure added.
    	- "sched~" functions are declaired.
    
    in "lab1_sched.c" :
    	- added student infomations.
    	- started defining the "sched~" functions.
    
    in "lab1_sched_test.c" :
    	- added student infomations.
    	- "main" function is edited to use PROCESS structure.
    
    other:
    	- Test file(test.txt) is deleted.
    
    * in "Makefile" :
    	- student infos edited(New team member!).
    
    in "lab1_sched_types.h" :
    	- some declarations are edited.
    	- args of functions are edited.
    	- new variable to structure PROCESS(int priority).
    
    in "lab1_sched_test.c" :
    	- NEW global variables!
    	- "insert_process" function added.
    
    in "lab1_sched.c"
    	- NEW global variables!
    	- "sched_RoundRobin" function is DONE!!
    	- "RRpop" function is DONE!!
    	- "RRpush" function is DONE!!
    		>>this will be used for other functions too.
    	- "allocate_memory", "initialize_memory", "copy_data" functions added.

--- lab1_sched/include/lab1_sched_types.h
+++ lab1_sched/include/lab1_sched_types.h
@@ -1,12 +1,13 @@
 /*
 *	DKU Operating System Lab
 *	    Lab1 (Scheduler Algorithm Simulator)
-*	    Student id : 
-*	    Student name : 
+*	    Student id : 32170896, 32163395, 32162925
+*	    Student name : Kim EunJin, Lee WonJoo, Yun SeulGi
 *
 *   lab1_sched_types.h :
-*       - lab1 header file.
-*       - must contains scueduler algorithm function's declations.
+*       - Lab1 header file.
+*       - Must contain scheduler algorithm function's declations.
+*       + ALSO contains structure infomation used in the functions.
 *
 */
 
@@ -18,8 +19,35 @@
  * You need to Declare functions in  here
  */
 
+typedef struct process_ {
+	//name of the structure is set as "process_" for just in case
+	char name;
+	int t_arrival;
+	int t_service;
+	int t_process; //for checking remaining servicetime
+	int priority; //this is used in Lottery sched!!
+}PROCESS;
 
-#endif /* LAB1_HEADER_H*/
+int sched_FIFO(PROCESS* p_array[]);
+int sched_RoundRobin(PROCESS* p_array[]);
+int sched_MLFQ(PROCESS* p_array[]);
+int sched_Lottery(PROCESS* p_array[]);
+
+//for Dinamic allocation of memory in RR, MLFQ, ...
+void allocate_memory(PROCESS* array[]);
+
+void initialize_memory(PROCESS* array[]); //initialize memory to NULL
+int copy_data(PROCESS** p1, PROCESS** p2); //copying the data
+
+void print_sched(); //how?
+
+//these funcs will be turned into void funcs at the end of the project(not fixed)
+int RRpop(PROCESS* Queue[]); //this is used in rr
+int RRpush(PROCESS* Queue[], PROCESS* p_array[], PROCESS* temp, int index); //this is used in rr
 
 
+
+/*Do we need to >>>>sort the workloads<<<< in t_arrival order? */
+
+#endif /* _LAB1_HEADER_H*/
 
--- lab1_sched/lab1_sched_test.c
+++ lab1_sched/lab1_sched_test.c
@@ -1,19 +1,19 @@
 /*
 *	DKU Operating System Lab
 *	    Lab1 (Scheduler Algorithm Simulator)
-*	    Student id : 
-*	    Student name : 
+*	    Student id : 32170896, 32163395, 32162925
+*	    Student name : Kim EunJin, Lee WonJoo, Yun SeulGi
 *
 *   lab1_sched.c :
 *       - Lab1 source file.
-*       - Must contains scueduler algorithm test code.
+*       - Must contain scheduler algorithm test code.
 *
 */
 
 #include <aio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+//#include <fcntl.h> - Why included for 2 times?
 #include <errno.h>
 #include <time.h>
 #include <sys/time.h>
@@ -28,12 +28,76 @@
 
 #include "lab1_sched_types.h"
 
+int** result = NULL;
+extern int process_num; //for number of processes
+extern int t_total; //for number of processes
+extern int head;
+extern int tail;
+extern char* char_result;
+
 /*
  * you need to implement scheduler simlator test code.
  *
  */
 
-int main(int argc, char *argv[]){
+//function for inserting processe info's into "p_array"
+void insert_process(PROCESS* p_array[]);
 
+int main(int argc, char *argv[]){
+	//PROCESS *p_array; //struct process_
+
+	//int i; //for loops
+
+	printf("**** Scheduler simulator - FIFO, RR, MLFQ, Lottery ****\n");
+	printf("Number of processes? : ");
+	scanf("%d", &process_num);
+
+	const int p_num = process_num; //to prevent any changes to process_num
+	PROCESS *p_array[p_num]; //array for PROCESS*(struct process_*)
+
+printf("main ");
+
+	//Dinamic allocation and initialization is done in insertProcess()
+	insert_process(p_array);
+
+	char_result = (char*)calloc(t_total, sizeof(char));
+	
+	/* Now calls all the scheduling simulator */
+	/* You can modify the args of the functions */
+
+	//sched_FIFO(p_array);
+	sched_RoundRobin(p_array);
+	//sched_MLFQ(p_array);
+	//sched_Lottery(p_array);
+
+	return 0;
 }
 
+void insert_process(PROCESS* p_array[])
+{
+	//changes the head and tail value
+	int i; //for loops
+	const int p_num = process_num; //to prevent any changes to process_num
+	
+printf("allocate ");	
+	allocate_memory(p_array);
+
+	//Initialization process
+	for(i=0; i<p_num; i++) {
+		getchar(); //emptys the buffer
+		printf("\nInput the name(single char) of %dth process : ", i);
+		scanf("%c", &p_array[i]->name);
+		printf("Input arrival time of %c : ", p_array[i]->name);
+		scanf("%d", &p_array[i]->t_arrival);
+		printf("Input service time of %c : ", p_array[i]->name);
+		scanf("%d", &p_array[i]->t_service);
+		printf("Input priority of %c (for Lotterty sched) : ", p_array[i]->name);
+		scanf("%d", &p_array[i]->priority);
+		//t_process is always initialize with the value of t_service
+		p_array[i]->t_process = p_array[i]->t_service;
+		t_total += p_array[i]->t_service;
+	}
+
+printf("initialize done ");	
+	return;
+}
