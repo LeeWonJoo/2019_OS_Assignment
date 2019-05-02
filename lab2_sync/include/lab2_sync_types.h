120e6544cfd60c1083d9fb9dc66665c7111a0d45
WonJoo Lee <wonjoo12345@naver.com>
2019-05-02 23:41:53 +0900

    in "lab2_sync_types.h": 	- return type of "lab2_node_delete" is changed from <void> to <int>. 	- added func "lab2_node_inorder" 		-this func visits nodes 		-calls itself in "lab2_bst.c" : 	- func "lab2_node_print_inorder" is declared 	- funcs "lab2_tree~" are declared 	- funcs "lab2_node_create/delete" are declared 	- funcs "lab2_node_insert~" are declared 		-all these funcs need test execution!! 	- funcs "lab2_node_remove~" are in progress

--- lab2_sync/include/lab2_sync_types.h
+++ lab2_sync/include/lab2_sync_types.h
@@ -27,18 +27,18 @@
  */
 typedef struct lab2_node {
     pthread_mutex_t mutex;
-    struct lab2_node *left;
-    struct lab2_node *right;
+    struct lab2_node* left;
+    struct lab2_node* right;
     int key;
 } lab2_node;
 
 /*
  * lab2_tree
  *
- *  struct lab2_node *root  : root node of bst.
+ *  struct lab2_node* root  : root node of bst.
  */
 typedef struct lab2_tree {
-    struct lab2_node *root;
+    struct lab2_node* root;
 } lab2_tree;
 
 /* 
@@ -46,11 +46,11 @@ typedef struct lab2_tree {
  */
 typedef struct thread_arg{
     pthread_t thread;
-    lab2_tree *tree;
+    lab2_tree* tree;
     int node_count;
     int num_iterations;
     int is_sync;
-    int *data_set;
+    int* data_set;
     int start;
     int end;
 }thread_arg;
@@ -60,17 +60,22 @@ typedef struct thread_arg{
  * TODO
  *  You need to implement these functions. 
  */
-int lab2_node_print_inorder(lab2_tree *tree);
-lab2_tree *lab2_tree_create();
-lab2_node *lab2_node_create(int key);
-int lab2_node_insert(lab2_tree *tree, lab2_node *new_node);
-int lab2_node_insert_fg(lab2_tree *tree, lab2_node *new_node);
-int lab2_node_insert_cg(lab2_tree *tree, lab2_node *new_node);
-int lab2_node_remove(lab2_tree *tree, int key);
-int lab2_node_remove_fg(lab2_tree *tree, int key);
-int lab2_node_remove_cg(lab2_tree *tree, int key);
-void lab2_tree_delete(lab2_tree *tree);
-void lab2_node_delete(lab2_node *node);
+int lab2_node_print_inorder(lab2_tree* tree);
+int lab2_node_inorder(lab2_node* node);
+
+lab2_tree* lab2_tree_create();
+void lab2_tree_delete(lab2_tree* tree);
+lab2_node* lab2_node_create(int key);
+int lab2_node_delete(lab2_node* node);
+
+int lab2_node_insert(lab2_tree* tree, lab2_node* new_node);
+int lab2_node_insert_fg(lab2_tree* tree, lab2_node* new_node);
+int lab2_node_insert_cg(lab2_tree* tree, lab2_node* new_node);
+
+int lab2_node_remove(lab2_tree* tree, int key);
+int lab2_node_remove_fg(lab2_tree* tree, int key);
+int lab2_node_remove_cg(lab2_tree* tree, int key);
+
 
 
 /*
@@ -81,21 +86,21 @@ void lab2_node_delete(lab2_node *node);
  */
 
 /* assembly implementation example functions */
-void atomic_add( int * value, int inc_val);
-void atomic_sub( int * value, int dec_val);
-void atomic_inc( int * value);
-void atomic_dec( int * value);
+void atomic_add( int* value, int inc_val);
+void atomic_sub( int* value, int dec_val);
+void atomic_inc( int* value);
+void atomic_dec( int* value);
 
 /*
  * lab2_bonus.c related functions 
  * TODO
  *  You need to implement these function.
  */
-void lab2_spin_lock(int volatile *lock);
-void lab2_spin_unlock(int volatile *lock);
+void lab2_spin_lock(int volatile* lock);
+void lab2_spin_unlock(int volatile* lock);
 
 
 /* lab2_timeval.c related function */
-double get_timeval(struct timeval *tv, struct timeval *tv_end);
+double get_timeval(struct timeval* tv, struct timeval* tv_end);
 
 #endif /* LAB2_HEADER_H*/
--- lab2_sync/lab2_bst.c
+++ lab2_sync/lab2_bst.c
@@ -26,9 +26,24 @@
  *  @param lab2_tree *tree  : bst to print in-order. 
  *  @return                 : status (success or fail)
  */
-int lab2_node_print_inorder(lab2_tree *tree)
+int lab2_node_print_inorder(lab2_tree* tree)
 {
     // You need to implement lab2_node_print_inorder function.
+	int node_cnt = 0;
+	node_cnt = lab2_node_inorder(tree->root);
+	return node_cnt;
+}
+
+int lab2_node_inorder(lab2_node* node)
+{
+	static int node_count = 0;
+	if(node) {
+		node_count++;
+		lab2_node_inorder(node->left);
+		printf("%d ", node->key);
+		lab2_node_inorder(node->right);
+	}
+	return node_count;
 }
 
 
@@ -39,9 +54,28 @@ int lab2_node_print_inorder(lab2_tree *tree)
  * 
  *  @return                 : bst which you created in this function.
  */
-lab2_tree *lab2_tree_create()
+lab2_tree* lab2_tree_create()
 {
     // You need to implement lab2_tree_create function.
+	lab2_tree* newTree = (lab2_tree*)malloc(sizeof(lab2_tree));
+	newTree->root = NULL;
+	return newTree;
+}
+
+
+/*
+ * TODO
+ *  Implement function which delete struct lab2_tree
+ *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
+ *
+ *  @param lab2_tree *tree  : bst which you want to delete. 
+ *  @return                 : status(success or fail)
+ */
+void lab2_tree_delete(lab2_tree* tree)
+{
+    // You need to implement lab2_tree_delete function.
+	free(tree);
+	return;
 }
 
 
@@ -53,9 +87,34 @@ lab2_tree *lab2_tree_create()
  *  @param int key          : bst node's key to creates
  *  @return                 : bst node which you created in this function.
  */
-lab2_node * lab2_node_create(int key)
+lab2_node* lab2_node_create(int key)
 {
-    // You need to implement lab2_node_create function.
+	// You need to implement lab2_node_create function.
+	lab2_node* newNode = (lab2_node*)malloc(sizeof(lab2_node));
+	int check = pthread_mutex_init(&(newNode->mutex), NULL);
+	assert(check == 0); //checking success
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->key = key;
+
+	return newNode;
+}
+
+
+/*
+ * TODO
+ *  Implement function which delete struct lab2_node
+ *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
+ *
+ *  @param lab2_node* node  : bst node which you want to remove. 
+ *  @return                     : status (success or fail)
+ */
+int lab2_node_delete(lab2_node* node)
+{
+	// You need to implement lab2_node_delete function.
+	pthread_mutex_destroy(&(node->mutex));
+	free(node);
+	return LAB2_SUCCESS;
 }
 
 
@@ -67,9 +126,36 @@ lab2_node * lab2_node_create(int key)
  *  @param lab2_node *new_node  : bst node which you need to insert. 
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert(lab2_tree *tree, lab2_node *new_node)
+int lab2_node_insert(lab2_tree* tree, lab2_node* new_node)
 {
-    // You need to implement lab2_node_insert function.
+	// You need to implement lab2_node_insert function.
+	lab2_node* nextNode = tree->root;
+	lab2_node* currNode;
+	int isLeft = 1;
+
+	if(tree->root == NULL) {
+		tree->root = new_node;
+	}
+	else {
+		while(nextNode != NULL) {
+			currNode = nextNode;
+			if(currNode->key <= new_node->key) {
+				nextNode = currNode->left;
+				isLeft = 1;
+			}
+			else {
+				nextNode = currNode->right;
+				isLeft = 0;
+			}
+		}//breaks when nextNode == NULL ->now time to insert!!
+		if(isLeft) {
+			currNode->left = new_node;
+		}
+		else {
+			currNode->right = new_node;
+		}
+	}
+	return LAB2_SUCCESS;
 }
 
 
@@ -81,9 +167,46 @@ int lab2_node_insert(lab2_tree *tree, lab2_node *new_node)
  *  @param lab2_node *new_node  : bst node which you need to insert. 
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert_fg(lab2_tree *tree, lab2_node *new_node)
+int lab2_node_insert_fg(lab2_tree* tree, lab2_node* new_node)
 {
-      // You need to implement lab2_node_insert_fg function.
+	// You need to implement lab2_node_insert_fg function.
+	lab2_node* nextNode = tree->root;
+	lab2_node* currNode;
+	int isLeft = 1;
+
+	if(tree->root == NULL) {
+		tree->root = new_node;
+	}
+	else {
+		while(1) {
+			currNode = nextNode;
+			//make lock and check this node
+			pthread_mutex_lock(&(currNode->mutex));
+			if(currNode->key <= new_node->key) {
+				nextNode = currNode->left;
+				isLeft = 1;
+			}
+			else {
+				nextNode = currNode->right;
+				isLeft = 0;
+			}
+			if(nextNode != NULL)
+				pthread_mutex_unlock(&(currNode->mutex));
+			else
+				break;
+		}//breaks when nextNode == NULL
+		//now time to insert!!
+		//we still have lock for currNode
+		if(isLeft) {
+			currNode->left = new_node;
+		}
+		else {
+			currNode->right = new_node;
+		}
+		//now unlocks currNode
+		pthread_mutex_unlock(&(currNode->mutex));
+	}
+	return LAB2_SUCCESS;
 }
 
 
@@ -95,9 +218,38 @@ int lab2_node_insert_fg(lab2_tree *tree, lab2_node *new_node)
  *  @param lab2_node *new_node  : bst node which you need to insert. 
  *  @return                     : status (success or fail)
  */
-int lab2_node_insert_cg(lab2_tree *tree, lab2_node *new_node)
+int lab2_node_insert_cg(lab2_tree* tree, lab2_node* new_node)
 {
-    // You need to implement lab2_node_insert_cg function.
+	// You need to implement lab2_node_insert_cg function.
+	lab2_node* nextNode = tree->root;
+	lab2_node* currNode;
+	int isLeft = 1;
+
+	if(tree->root == NULL) {
+		tree->root = new_node;
+	}
+	else {
+		pthread_mutex_lock(&(tree->root->mutex));
+		while(nextNode != NULL) {
+			currNode = nextNode;
+			if(currNode->key <= new_node->key) {
+				nextNode = currNode->left;
+				isLeft = 1;
+			}
+			else {
+				nextNode = currNode->right;
+				isLeft = 0;
+			}
+		}//breaks when nextNode == NULL ->now time to insert!!
+		if(isLeft) {
+			currNode->left = new_node;
+		}
+		else {
+			currNode->right = new_node;
+		}
+		pthread_mutex_unlock(&(tree->root->mutex));
+	}
+	return 0;
 }
 
 
@@ -109,9 +261,51 @@ int lab2_node_insert_cg(lab2_tree *tree, lab2_node *new_node)
  *  @param int key          : key value that you want to delete. 
  *  @return                 : status (success or fail)
  */
-int lab2_node_remove(lab2_tree *tree, int key)
+int lab2_node_remove(lab2_tree* tree, int key)
 {
-    // You need to implement lab2_node_remove function.
+	// You need to implement lab2_node_remove function.
+	lab2_node* nextNode = tree->root;
+	lab2_node* currNode = NULL;
+	lab2_node* pastNode = NULL;
+
+	lab2_node* tmp1 = NULL;
+	lab2_node* tmp2 = NULL;
+	int isFound=0;
+
+	if(tree->root == NULL) {
+		return LAB2_ERROR;
+	}
+	else {
+		while(nextNode != NULL) {
+			pastNode = currNode;
+			currNode = nextNode;
+			//make lock and check this node
+			pthread_mutex_lock(&(currNode->mutex));
+			if(currNode->key < key) {
+				nextNode = currNode->left;
+			}
+			else if(currNode->key > key){
+				nextNode = currNode->right;
+			}
+			else {
+				isFound=1;
+				break;
+			}
+		}//breaks when nextNode == NULL or currNode->kew == key
+		//now time to delete!!
+		if(!isFound) {
+			//when nextNode == NULL
+			//means, matching node not found
+			return LAB2_ERROR;
+		}
+		else { //matching node is found
+			tmp1 = currNode->left;
+			tmp2 = currNode->right;
+			lab2_node_delete(currNode);
+			
+		}
+	}
+return LAB2_SUCCESS;
 }
 
 
@@ -123,7 +317,7 @@ int lab2_node_remove(lab2_tree *tree, int key)
  *  @param int key          : key value that you want to delete. 
  *  @return                 : status (success or fail)
  */
-int lab2_node_remove_fg(lab2_tree *tree, int key)
+int lab2_node_remove_fg(lab2_tree* tree, int key)
 {
     // You need to implement lab2_node_remove_fg function.
 }
@@ -137,36 +331,7 @@ int lab2_node_remove_fg(lab2_tree *tree, int key)
  *  @param int key          : key value that you want to delete. 
  *  @return                 : status (success or fail)
  */
-int lab2_node_remove_cg(lab2_tree *tree, int key)
+int lab2_node_remove_cg(lab2_tree* tree, int key)
 {
     // You need to implement lab2_node_remove_cg function.
 }
-
-
-/*
- * TODO
- *  Implement function which delete struct lab2_tree
- *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
- *
- *  @param lab2_tree *tree  : bst which you want to delete. 
- *  @return                 : status(success or fail)
- */
-void lab2_tree_delete(lab2_tree *tree)
-{
-    // You need to implement lab2_tree_delete function.
-}
-
-
-/*
- * TODO
- *  Implement function which delete struct lab2_node
- *  ( refer to the ./include/lab2_sync_types.h for structure lab2_node )
- *
- *  @param lab2_tree *tree  : bst node which you want to remove. 
- *  @return                 : status(success or fail)
- */
-void lab2_node_delete(lab2_node *node)
-{
-    // You need to implement lab2_node_delete function.
-}
-
